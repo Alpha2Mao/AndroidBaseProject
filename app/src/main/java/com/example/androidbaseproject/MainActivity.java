da8be3aad84babae176ff14e80c6d566c3c6ddaf
Alpha2Mao <2328119758@qq.com>
2020-11-25 00:07:28 +0800

    add bottom navigation view

--- app/src/main/java/com/example/androidbaseproject/MainActivity.java
+++ app/src/main/java/com/example/androidbaseproject/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.androidbaseproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.nav_view);
+        navigation.setItemIconTintList(null);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.mainFragment, R.id.secondFragment
+        ).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigation, navController);
+
     }
 }
